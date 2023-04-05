db04fa83576907e29f9a46db31bab9a9bcd1805e
GiuliaPicasso <giulia.picasso@mail.polimi.it>
2023-04-05 13:54:25 +0900

    mn

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-var myvideo = document.getElementById("video1")
-function playpause(){
-  if(myvideo.paused){
-    myvideo.play();
-    document.getElementById("playpause_img").style.backgroundImage = "none";
-  }
-  else {
-    myvideo.pause()
-    document.getElementById("playpause_img").style.backgroundImage = "url('images/playbtn.png')";
-  }
-}
+
